a7d2c7bd4fb9158c3fd383fa5df997e7a5cbb90b
Kamil Duljas <kduljas@solwit.com>
2023-02-16 22:02:54 +0100

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -4,15 +4,7 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  Author: Kamil Duljas	kamil.duljas@gmail.com
   ******************************************************************************
   */
 /* USER CODE END Header */
